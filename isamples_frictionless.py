c98b63a107aa5ee425540ec614191023ecc80577
Danny Mandel <danny.mandel@gmail.com>
2022-10-04 14:19:29 -0700

    Make schema a method parameter constructed separately from the package

--- isamples_frictionless.py
+++ isamples_frictionless.py
@@ -1,131 +1,29 @@
-from frictionless import Package, Resource
+import json
+from typing import Optional
 
-SCHEMA_JSON = {
-  "fields": [
-    {
-      "type": "string",
-      "name": "id"
-    },
-    {
-      "type": "string",
-      "name": "label"
-    },
-    {
-      "type": "string",
-      "name": "description"
-    },
-    {
-      "type": "array",
-      "name": "hasContextCategory"
-    },
-    {
-      "type": "array",
-      "name": "hasMaterialCategory"
-    },
-    {
-      "type": "array",
-      "name": "hasSpecimenCategory"
-    },
-    {
-      "type": "array",
-      "name": "keywords"
-    },
-    {
-      "type": "array",
-      "name": "informalClassification"
-    },
-    {
-      "type": "string",
-      "name": "producedBy_label"
-    },
-    {
-      "type": "string",
-      "name": "producedBy_description"
-    },
-    {
-      "type": "string",
-      "name": "producedBy_hasFeatureOfInterest"
-    },
-    {
-      "type": "array",
-      "name": "producedBy_responsibility"
-    },
-    {
-      "type": "datetime",
-      "name": "producedBy_resultTime"
-    },
-    {
-      "type": "string",
-      "name": "producedBy_samplingSite_description"
-    },
-    {
-      "type": "string",
-      "name": "producedBy_samplingSite_label"
-    },
-    {
-      "type": "integer",
-      "name": "producedBy_samplingSite_location_elevationInMeters"
-    },
-    {
-      "type": "number",
-      "name": "producedBy_samplingSite_location_latitude"
-    },
-    {
-      "type": "number",
-      "name": "producedBy_samplingSite_location_longitude"
-    },
-    {
-      "type": "array",
-      "name": "producedBy_samplingSite_placeName"
-    },
-    {
-      "type": "array",
-      "name": "registrant"
-    },
-    {
-      "type": "array",
-      "name": "samplingPurpose"
-    },
-    {
-      "type": "string",
-      "name": "curation_label"
-    },
-    {
-      "type": "string",
-      "name": "curation_description"
-    },
-    {
-      "type": "string",
-      "name": "curation_accessContraints"
-    },
-    {
-      "type": "string",
-      "name": "curation_location"
-    },
-    {
-      "type": "string",
-      "name": "curation_responsibility"
-    },
-    {
-      "type": "array",
-      "name": "compliesWith"
-    },
-    {
-      "type": "array",
-      "name": "authorizedBy"
-    }
-  ]
-}
+from frictionless import Package, Resource, Schema, FrictionlessException
+
+DEFAULT_SCHEMA_FILE_NAME = "isamples_simple_schema.json"
 
 
-def create_isamples_package(file_path: str) -> Package:
-  """
-  Opens the specified file and return it as a list of iSamples Core record dictionaries
-  Args:
-      file_path: The path to the file to open
+def check_valid_schema_json(schema_file_path: str) -> Optional[Schema]:
+    try:
+        schema = Schema(schema_file_path)
+        return schema
+    except FrictionlessException as e:
+        print(f"Error constructing frictionless schema: {e}")
+        return None
 
-  Returns: A list of dictionaries containing the records
-  """
-  data_resource = Resource(source=file_path, schema=SCHEMA_JSON, trusted=True)
-  package = Package(resources=[data_resource], name="isamples", title="isamples", id="isamples", trusted=True)
-  return package
+
+def create_isamples_package(schema: Schema, file_path: str) -> Package:
+    """
+    Opens the specified file and return it as a Frictionless Package
+    Args:
+        schema: The frictionless Schema to use to open the file
+        file_path: The path to the data file to open
+
+    Returns: A frictionless package representing the file at the specified file path
+    """
+    data_resource = Resource(source=file_path, schema=schema, trusted=True)
+    package = Package(resources=[data_resource], name="isamples", title="isamples", id="isamples", trusted=True)
+    return package
