ce989efe34f0b1198a9db3fce7fd075b9f519ce5
Danny Mandel <danny.mandel@gmail.com>
2022-10-05 12:30:21 -0700

    Adapt API to use in iSB

--- isamples_frictionless.py
+++ isamples_frictionless.py
@@ -1,11 +1,12 @@
 import json
 from typing import Optional
-
+from pathlib import Path
+import os.path
 from frictionless import Package, Resource, Schema, FrictionlessException, Report
 from tabulate import tabulate
 
 DEFAULT_SCHEMA_FILE_NAME = "isamples_simple_schema.json"
-
+ISAMPLES_SIMPLE_SCHEMA = None
 
 def check_valid_schema_json(schema_file_path: str) -> Optional[Schema]:
     try:
@@ -28,6 +29,20 @@ def create_isamples_package(schema: Schema, file_path: str) -> Package:
     data_resource = Resource(source=file_path, schema=schema, trusted=True)
     package = Package(resources=[data_resource], name="isamples", title="isamples", id="isamples", trusted=True)
     return package
+
+
+def isamples_simple_schema() -> Schema:
+    """
+    Constructs the iSamples built-in schema using the bundled JSON
+    Returns: The frictionless iSamples simple Schema
+    """
+    global ISAMPLES_SIMPLE_SCHEMA
+    if ISAMPLES_SIMPLE_SCHEMA is None:
+        p = Path(__file__)
+        SCHEMA_JSON = {}
+        schema_json_path = os.path.join(p.parent, "isamples_simple_schema.json")
+        ISAMPLES_SIMPLE_SCHEMA = check_valid_schema_json(schema_json_path)
+    return ISAMPLES_SIMPLE_SCHEMA
 
 
 def report_errors_as_str(report: Report) -> str:
