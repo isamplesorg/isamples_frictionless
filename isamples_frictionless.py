e8323f4c5a07c90d40a36fdf082ada320f722654
Danny Mandel <danny.mandel@gmail.com>
2022-10-05 15:05:37 -0700

    Think we finally have this right

---
+++ isamples_frictionless.py
@@ -0,0 +1,3 @@
+# This file is deliberately empty and here to trick poetry's build system into agreeing with our structure.
+# Previously the full contents of the module lived in this file (rather than __init__.py), but there were
+# problems with the imports both in mypy and flake8.  This approach appears to satisfy all the robot overlords.
