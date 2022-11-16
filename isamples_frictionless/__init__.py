857681a160b444511bde6e998481099a9ded6441
Danny Mandel <danny.mandel@gmail.com>
2022-11-16 11:54:01 -0700

    New functionality to insert identifiers into a Resource

--- isamples_frictionless/__init__.py
+++ isamples_frictionless/__init__.py
@@ -1,11 +1,14 @@
 from typing import Optional
 from pathlib import Path
 import os.path
-from frictionless import Package, Resource, Schema, FrictionlessException, Report
+from frictionless import Package, Pipeline, Resource, Schema, Step, FrictionlessException, Report, transform, steps, \
+    validate
 from tabulate import tabulate
 
 DEFAULT_SCHEMA_FILE_NAME = "isamples_simple_schema.json"
 ISAMPLES_SIMPLE_SCHEMA = None
+DEFAULT_TEMPLATE_FILE_NAME = "isamples_simple_template.csv"
+ISAMPLES_SIMPLE_TEMPLATE = None
 
 
 def check_valid_schema_json(schema_file_path: str) -> Optional[Schema]:
@@ -39,11 +42,63 @@ def isamples_simple_schema() -> Schema:
     global ISAMPLES_SIMPLE_SCHEMA
     if ISAMPLES_SIMPLE_SCHEMA is None:
         p = Path(__file__)
-        schema_json_path = os.path.join(p.parent, "isamples_simple_schema.json")
+        schema_json_path = os.path.join(p.parent, DEFAULT_SCHEMA_FILE_NAME)
         ISAMPLES_SIMPLE_SCHEMA = check_valid_schema_json(schema_json_path)
     return ISAMPLES_SIMPLE_SCHEMA
+
+
+def isamples_simple_template() -> Package:
+    global ISAMPLES_SIMPLE_TEMPLATE
+    if ISAMPLES_SIMPLE_TEMPLATE is None:
+        p = Path(__file__)
+        template_file_path = os.path.join(p.parent, DEFAULT_TEMPLATE_FILE_NAME)
+        ISAMPLES_SIMPLE_TEMPLATE = create_isamples_package(isamples_simple_schema(), template_file_path)
+    return ISAMPLES_SIMPLE_TEMPLATE
 
 
 def report_errors_as_str(report: Report) -> str:
     errors = report.flatten(['code', 'message'])
     return tabulate(errors, headers=['code', 'message'])
+
+
+class _PopulateIdentifiersStep(Step):
+    def __init__(self, identifiers: list[str]):
+        super().__init__()
+        self._identifiers = identifiers
+
+    def transform_resource(self, resource):
+        current = resource.to_copy()
+
+        # This function is called to replace the data contained in the resource.
+        def data():
+            with current:
+                column_length = 0
+                index = 0
+                template_row = None
+                # The first two rows in the template are the column titles and a placeholder row.
+                for list in current.list_stream:
+                    if index == 0:
+                        # Column titles, return as-is.
+                        column_length = len(list)
+                        yield list
+                    elif index == 1:
+                        # Placeholder row, save it for later to use when we populate the latter rows
+                        template_row = list
+                    index += 1
+                # Now add a row for each identifier
+                for identifier in self._identifiers:
+                    empty_row_with_identifier = template_row.copy()
+                    # Replace the identifier column (always going to be the first column) with the current id
+                    empty_row_with_identifier[0] = identifier
+                    # Then yield the modified list
+                    yield empty_row_with_identifier
+
+        # Replace the data by setting a custom function that generates the data.
+        resource.data = data
+
+def insert_identifiers_into_template(identifiers: list[str]):
+    source = isamples_simple_template()
+    target = transform(source.resources[0], steps=[_PopulateIdentifiersStep(identifiers)])
+    print()
+    # print(target.schema)
+    print(target.to_view())
---
+++ tests/test_isamples_frictionless.py
@@ -0,0 +1,5 @@
+import isamples_frictionless
+
+
+def test_insert_identifiers_into_template():
+    isamples_frictionless.insert_identifiers_into_template(["123", "456"])
